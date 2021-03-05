89ad90140e48ce716f5813bbd185baead65b4dd5
AlexWebDevelop <alexanderwolf417@gmail.com>
2021-03-05 14:40:28 +0300

    feat: final 3

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 // You should implement your task here.
 
+// module.exports = function towelSort(matrix) {
+//     if (matrix.length == 0) {
+//         return [];
+//     }
+//     let newArr = [];
+//     for (let i = 0; i < matrix.length; i++) {
+//         for (let j = 0; j < matrix[i].length; j++) {
+//             newArr.push(matrix[i][j]);
+//         }
+//     }
+//     newArr.sort(function (fisrt, second) {
+//         if (fisrt > second) return 1;
+//         if (second > fisrt) return -1;
+//         if (second == fisrt) return 0;
+//     });
+//     console.log(newArr);
+//     return newArr;
+// };
+
 module.exports = function towelSort(matrix) {
-    if (matrix.length == 0) {
-        return [];
+    if (!Array.isArray(matrix) || matrix.length <= 0) return [];
+    for (let i = 1; i < matrix.length; i += 2) {
+        matrix[i].reverse();
     }
-    let newArr = [];
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            newArr.push(matrix[i][j]);
-        }
-    }
-    newArr.sort(function (fisrt, second) {
-        if (fisrt > second) return 1;
-        if (second > fisrt) return -1;
-        if (second == fisrt) return 0;
-    });
-    console.log(newArr);
-    return newArr;
+
+    return matrix.flat();
 };
