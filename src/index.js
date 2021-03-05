2abbc2c96aa5b29f62f3d7e211b45d961ba89a77
AlexWebDevelop <alexanderwolf417@gmail.com>
2021-03-05 14:43:52 +0300

    feat: final 4

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@
 // };
 
 module.exports = function towelSort(matrix) {
-    if (!Array.isArray(matrix) || matrix.length <= 0) return [];
+    if (!Array.isArray(matrix) || matrix.length == 0) return [];
     for (let i = 1; i < matrix.length; i += 2) {
         matrix[i].reverse();
     }
